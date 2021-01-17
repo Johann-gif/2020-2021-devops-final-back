41b63710e5b751f7461fb9bc036ed3af7326f46c
Johann <johann.de-almeida@lacatholille.fr>
2021-01-17 20:02:05 +0100

    :wrench: Modification settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -28,6 +28,7 @@ class Config(object):
         'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://tp-devops-final-front.netlify.app/',
+        'http://tp-devops-final-front.netlify.app/',
         os.environ.get('FRONT_PROD_URL', ''),
     ]
     JWT_HEADER_TYPE = 'Token'
